781db8150a9ac11ecdda75b7b32605fa8ec2dfad
synt000 <syntempo8@gmail.com@gmail.com>
2026-07-06 03:29:55 +0630

    Add CSS and Input Form

--- apps/main.py
+++ apps/main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from pathlib import Path
 
-app = FastAPI(title="Business OS")
+app = FastAPI()
+
+# CSS ဖိုင်တွေကို Static အနေနဲ့ သတ်မှတ်မယ်
+app.mount("/static", StaticFiles(directory="apps/templates"), name="static")
 
 @app.get("/", response_class=HTMLResponse)
 async def root():
