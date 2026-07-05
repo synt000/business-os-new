fd944a939c7ce9848ffc795bc13d1ceec998366b
synt000 <syntempo8@gmail.com@gmail.com>
2026-07-06 03:37:12 +0630

    Add form processing logic

--- apps/main.py
+++ apps/main.py
@@ -1,14 +1,16 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Form
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
-from pathlib import Path
 
 app = FastAPI()
 
-# CSS ဖိုင်တွေကို Static အနေနဲ့ သတ်မှတ်မယ်
 app.mount("/static", StaticFiles(directory="apps/templates"), name="static")
 
 @app.get("/", response_class=HTMLResponse)
 async def root():
     with open("apps/templates/index.html", "r", encoding="utf-8") as f:
         return f.read()
+
+@app.post("/start")
+async def start_os(os_name: str = Form(...)):
+    return {"message": f"Welcome to {os_name}, တောဘုရင်ရဲ့ နယ်မြေက မင်းကို ကြိုဆိုပါတယ်!"}
